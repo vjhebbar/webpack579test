0871f96ba8e02b7f18e594af5aba8a81c2d01ca1
Vijay Hebbar <vijayhebbar91@gmail.com>
2024-01-24 19:06:16 -0500

    webpack 5.78 with destructuring

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import { Colors } from "./colors"
 function printComponents() {
     console.log(Colors.Red);
     console.log(Colors.Green);
-    // const { Red, Green } = Colors;
-    console.log(Colors.Red);
-    console.log(Colors.Green);
+    const { Red, Green } = Colors;
+    console.log(Red);
+    console.log(Green);
 }
 
 printComponents();
